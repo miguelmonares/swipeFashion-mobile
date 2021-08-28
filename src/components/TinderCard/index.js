aa71824a3880e22757fabff0edb4a0725ca04d0d
mxnares <69873782+mxnares@users.noreply.github.com>
2021-08-27 22:56:33 -0700

    custom app card component

---
+++ src/components/TinderCard/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {Text, Image, ImageBackground, View, StyleSheet} from 'react-native';
+
+const Card = props => {
+  const {name, image, bio} = props.user;
+  return (
+    <View style={styles.card}>
+      <ImageBackground
+        source={{
+          uri: image,
+        }}
+        style={styles.image}>
+        <View style={styles.cardInner}>
+          <Text style={styles.name}>{name}</Text>
+          <Text style={styles.bio}>{bio}</Text>
+        </View>
+      </ImageBackground>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  name: {fontSize: 30, color: 'white', fontWeight: 'bold'},
+  bio: {fontSize: 18, color: 'white', lineHeight: 25},
+  card: {
+    width: '95%',
+    height: '70%',
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 7,
+    },
+    shadowOpacity: 0.41,
+    shadowRadius: 9.11,
+
+    elevation: 14,
+  },
+  cardInner: {padding: 10},
+  image: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 10,
+    overflow: 'hidden',
+    justifyContent: 'flex-end',
+  },
+});
+export default Card;
