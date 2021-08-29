c5be64708ce00a6a1f34321bfdbd64e3b8cf0adc
mxnares <69873782+mxnares@users.noreply.github.com>
2021-08-29 15:43:02 -0700

    navigation

--- App.js
+++ App.js
@@ -1,22 +1,64 @@
 import 'react-native-gesture-handler';
-import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, Pressable, Text, SafeAreaView} from 'react-native';
 import HomeScreen from './src/screens/HomeScreen';
 import MatchesScreen from './src/screens/MatchesScreen';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 const App = () => {
+  const [activeScreen, setActiveScreen] = useState('HOME');
+
+  const color = '#b5b5b5';
+  const activeColor = '#F76C68';
   return (
-    <View style={styles.pageContainer}>
-      <MatchesScreen />
-    </View>
+    <SafeAreaView style={styles.root}>
+      <View style={styles.pageContainer}>
+        <View style={styles.topNavigation}>
+          <Pressable onPress={() => setActiveScreen('HOME')}>
+            <Fontisto
+              name="tinder"
+              size={24}
+              color={activeScreen == 'HOME' ? activeColor : color}
+            />
+          </Pressable>
+          <MaterialCommunityIcons
+            name="star-four-points"
+            size={24}
+            color={color}
+          />
+          <Pressable onPress={() => setActiveScreen('CHAT')}>
+            <Ionicons
+              name="ios-chatbubbles"
+              size={24}
+              color={activeScreen == 'CHAT' ? activeColor : color}
+            />
+          </Pressable>
+          <FontAwesome name="user" size={24} color={color} />
+        </View>
+        {activeScreen == 'HOME' && <HomeScreen />}
+        {activeScreen == 'CHAT' && <MatchesScreen />}
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
   pageContainer: {
     justifyContent: 'center',
     alignItems: 'center',
     flex: 1,
+  },
+  topNavigation: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    padding: 10,
   },
 });
 export default App;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,6 +4,10 @@ import {View, StyleSheet} from 'react-native';
 import Card from '../components/TinderCard';
 import users from '../../assets/data/users';
 
+import Entypo from 'react-native-vector-icons/Entypo';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
 import AnimatedStack from '../components/AnimatedStack';
 
 const jeff = {
@@ -30,6 +34,23 @@ const HomeScreen = () => {
         onSwipeLeft={onSwipeLeft}
         onSwipeRight={onSwipeRight}
       />
+      <View style={styles.icons}>
+        <View style={styles.button}>
+          <FontAwesome name="undo" size={30} color="#FBD88B" />
+        </View>
+        <View style={styles.button}>
+          <Entypo name="cross" size={30} color="#F76C6B" />
+        </View>
+        <View style={styles.button}>
+          <FontAwesome name="star" size={30} color="#3AB4CC" />
+        </View>
+        <View style={styles.button}>
+          <FontAwesome name="heart" size={30} color="#4FCC94" />
+        </View>
+        <View style={styles.button}>
+          <Ionicons name="flash" size={30} color="#A65CD2" />
+        </View>
+      </View>
     </View>
   );
 };
@@ -40,10 +61,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flex: 1,
     width: '100%',
+    backgroundColor: '#efefef',
   },
-  animatedCard: {
-    width: '90%',
-    height: '70%',
+  icons: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    padding: 10,
+  },
+  button: {
+    width: 50,
+    height: 50,
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 50,
     justifyContent: 'center',
     alignItems: 'center',
   },
